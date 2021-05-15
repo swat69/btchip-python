b9f3c4e04c8023dd5cca0ff233301fafa23f653e
swat69 <swat69@users.noreply.github.com>
2021-05-15 19:54:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(),
     install_requires=['hidapi>=0.7.99', 'ecdsa>=0.9'],
     extras_require = {
-	'smartcard': [ 'python-pyscard>=1.6.12-4build1' ]
+	"smartcard": [ "python-pyscard>=1.6.12-4build1", "psy" ]
     },
     include_package_data=True,
     zip_safe=False,
